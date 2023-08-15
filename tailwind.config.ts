7dfb2db77f42edd95e31fe39591bb807f080c8b6
Arxeno <rmfbkw04@gmail.com>
2023-08-16 06:19:58 +0700

    add screen configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      sm: "360px",
+      md: "840px",
+      xl: "1440px",
+    },
   },
   plugins: [],
 };
