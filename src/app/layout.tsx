1255553b24b7fefbc205a68fd9251f2aa8474c87
Arxeno <rmfbkw04@gmail.com>
2023-08-16 06:10:59 +0700

    fix: use poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
-  title: 'RISTEK Open Source'
-}
+  title: "RISTEK Open Source",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" style={{scrollBehavior:"smooth"}}>
-        <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      style={{ scrollBehavior: "smooth" }}
+    >
+      <body className={`${poppins.className}`}>{children}</body>
     </html>
-  )
+  );
 }
