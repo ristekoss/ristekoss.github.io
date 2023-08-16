172af7ea5ee93c93cae5c2d1c3c4918190d5f72b
Arxeno <rmfbkw04@gmail.com>
2023-08-16 08:44:13 +0700

    feat(products): add carousel

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+  imageSrc: string;
+  imageAlt: string;
+  imageWidth: number;
+  publishedYear: string;
+  platform: string;
+  description: string;
+  githubLink: string;
+}
+
+function ProductCard({
+  imageSrc,
+  imageAlt,
+  imageWidth,
+  publishedYear,
+  platform,
+  description,
+  githubLink,
+}: Props) {
+  return (
+    <div className="w-[300px] h-fit rounded-[20px] p-5 bg-white text-sm flex-shrink-0 flex-grow-0">
+      <div className="card-content flex flex-col gap-[15px]">
+        <div className="upper-content">
+          <div className="brand h-12 w-fit">
+            <Image
+              src={imageSrc}
+              alt={imageAlt}
+              height={48}
+              width={imageWidth}
+            />
+          </div>
+          <div className="card-text flex flex-col gap-1">
+            <div className="product-tags flex flex-row gap-1 py-1">
+              <div className="text-white bg-[#C424A3] rounded-[200px] font-bold w-fit py-1 px-3">
+                {publishedYear}
+              </div>
+              <div className="bg-[#64E6FB] rounded-[200px] w-fit py-1 px-3">
+                {platform}
+              </div>
+            </div>
+            <p className="text-base">{description}</p>
+          </div>
+        </div>
+
+        <div className="buttons-container ml-auto flex flex-row gap-3">
+          <button className="bg-[#C9CEFC] p-[10px] rounded-lg">
+            <Image
+              src="icons/github.svg"
+              alt=""
+              width={20}
+              height={20}
+            />
+          </button>
+
+          <button className="bg-[#C9CEFC] p-[10px] rounded-lg">
+            <Image
+              src="icons/box-arrow-up-right.svg"
+              alt=""
+              width={20}
+              height={20}
+            />
+          </button>
+
+          <button className="bg-[#C9CEFC] p-[10px] rounded-lg">
+            <Image
+              src="icons/code-slash.svg"
+              alt=""
+              width={20}
+              height={20}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/app/sections/OurProducts.tsx
+++ src/app/sections/OurProducts.tsx
@@ -1,7 +1,187 @@
+"use client";
+import { useRef, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import Image from "next/image";
+import Slider, { Settings } from "react-slick";
+
+interface Products {
+  imageSrc: string;
+  imageAlt: string;
+  imageWidth: number;
+  publishedYear: string;
+  platform: string;
+  description: string;
+  githubLink: string;
+}
+
 export default function OurProducts() {
-    return (
-      <div className="flex h-[var(--height-h-screen,800px)] items-start items-center justify-center" id="products">
-        OurProducts
+  const [productShowIndex, setProductShowIndex] = useState<number>(0);
+
+  const sliderRef = useRef<Slider | null>(null);
+
+  const productsInfo: Products[] = [
+    {
+      imageSrc: "icons/logo-sunjad.svg",
+      imageAlt: "Susun Jadwal",
+      imageWidth: 240,
+      publishedYear: "2020",
+      platform: "Web",
+      description:
+        "Universitas Indonesia's #1 student course planning app with over 500 thousand users across 14 faculties.",
+      githubLink: "https://www.github.com",
+    },
+    {
+      imageSrc: "icons/bikun-tracker.svg",
+      imageAlt: "Bikun Tracker",
+      imageWidth: 225,
+      publishedYear: "2023",
+      platform: "Web",
+      description:
+        "Experience the Convenience of Riding Bikun with Bikun Tracker! With Bikun Tracker, you no longer have to worry about when Bikun will come or stress about its arrival schedule.",
+      githubLink: "https://www.github.com",
+    },
+    {
+      imageSrc: "icons/ristek-link.svg",
+      imageAlt: "Ristek Link",
+      imageWidth: 188,
+      publishedYear: "2021",
+      platform: "Web",
+      description:
+        "World's #1 Easy to use, no-logins, customisable url shortener. 1 million shortened links!",
+      githubLink: "https://www.github.com",
+    },
+    {
+      imageSrc: "icons/ulas-kelas.svg",
+      imageAlt: "Ulas Kelas",
+      imageWidth: 240,
+      publishedYear: "2022",
+      platform: "Mobile",
+      description:
+        "Say goodbye to confusion and hello to informed decision-making. UlasKelas is here to be your clever companion to choose your courses!",
+      githubLink: "https://www.github.com",
+    },
+  ];
+
+  const tes: Products[] = [
+    {
+      imageSrc: "icons/logo-sunjad.svg",
+      imageAlt: "Susun Jadwal",
+      imageWidth: 240,
+      publishedYear: "2020",
+      platform: "Web",
+      description:
+        "Universitas Indonesia's #1 student course planning app with over 500 thousand users across 14 faculties.",
+      githubLink: "https://www.github.com",
+    },
+  ];
+
+  const settings: Settings = {
+    infinite: true,
+    slidesToShow: 1,
+    centerMode: true,
+    autoplay: false,
+    speed: 1000,
+    arrows: false,
+    beforeChange: (currentSlide, nextSlide) => {
+      setProductShowIndex(nextSlide);
+    },
+  };
+
+  const handleNextProduct = () => {
+    sliderRef.current?.slickNext();
+  };
+
+  const handlePrevProduct = () => {
+    sliderRef.current?.slickPrev();
+  };
+
+  return (
+    <div
+      id="products-container"
+      className="bg-[#3C317E] px-5 py-32 xl:px-16 xl:py-[156.5px] flex flex-col gap-10"
+    >
+      <h1 className="font-bold text-white text-4xl w-[320px] mx-auto text-center xl:hidden">
+        Explore Our Products!
+      </h1>
+
+      <div className="hidden xl:flex flex-row gap-10 justify-center items-center">
+        <h1 className="text-white font-bold text-center text-7xl">
+          Explore Our Products!
+        </h1>
+
+        <Image
+          src="assets/decor-2.svg"
+          alt=""
+          width={321}
+          height={107}
+        />
       </div>
-    )
+
+      <div id="carousel-card-product">
+        <Slider
+          {...settings}
+          ref={sliderRef}
+          className="w-[300px] h-[347px] mx-auto flex flex-row gap-3"
+        >
+          {productsInfo.map((product, index) => {
+            return (
+              <ProductCard
+                {...product}
+                key={index}
+              />
+            );
+          })}
+        </Slider>
+
+        <div
+          id="cards-navigation"
+          className="flex flex-row gap-11 justify-between"
+        >
+          <button
+            className="bg-white rounded-full w-16 h-16"
+            onClick={handlePrevProduct}
+          >
+            <Image
+              className="mx-auto"
+              src="icons/caret-left-fill.svg"
+              alt=""
+              width={52}
+              height={52}
+            />
+          </button>
+
+          <div
+            id="pager-carousel-products"
+            className="flex flex-row gap-3 justify-center items-center"
+          >
+            {productsInfo.map((product, index) => {
+              return (
+                <div
+                  className={`w-3 h-3 ${
+                    index == productShowIndex
+                      ? "bg-[#64E6FB]"
+                      : "bg-neutral-400"
+                  } rounded-full`}
+                ></div>
+              );
+            })}
+            {/* <div className="w-3 h-3 bg-[#64E6FB] rounded-full"></div> */}
+          </div>
+
+          <button
+            className="bg-white rounded-full w-16 h-16"
+            onClick={handleNextProduct}
+          >
+            <Image
+              className="mx-auto"
+              src="icons/caret-right-fill.svg"
+              alt=""
+              width={52}
+              height={52}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
